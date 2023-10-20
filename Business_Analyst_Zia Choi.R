99fcc7270d62e0e576f8d6f67eea1edc5add2f39
Zia <130864065+hello-zia@users.noreply.github.com>
2023-10-20 23:13:38 +0100

    Update Business_Analyst_Zia Choi.R

--- Business_Analyst_Zia Choi.R
+++ Business_Analyst_Zia Choi.R
@@ -6,13 +6,13 @@ class(df)
 df<-as.data.frame(df)
 str(df)
 
-# 3. Conduct data exploration to identify patterns and relationships across the dataset.
+# Conduct data exploration to identify patterns and relationships across the dataset.
 
 # Checking the Size of data 
 length(df) #15
 nrow(df) #4092
 
-# Checking for missing values: sum() to check NAs 
+# Check for missing values: sum() to check NAs 
 sum(is.na(df)) #10584
 
 # Check the structure of the data frame
@@ -26,7 +26,7 @@ unique(df$Industry)
 unique(df$Sector)
 unique(df$Revenue)
 
-# 4. Display the following columns: a. Job Title; b. Rating; c. Location; d. Industry
+# Display the following columns: a. Job Title; b. Rating; c. Location; d. Industry
 
 library(dplyr)
 
@@ -34,14 +34,14 @@ library(dplyr)
 selected_columns <- select(df, "Job Title", "Rating", "Location", "Industry")
 selected_columns
 
-# 5. Show a list of the following: 
+# SUMMARY 1: Show a list of the following: 
 # a. Top 20 Industries (must be unique values, no duplicates) 
 # b. Top 20 Sectors (must be unique values, no duplicates) 
 # c. Top 20 Headquarters (must be unique values, no duplicates) 
 
 library(dplyr)
 
-# Select the "Industry" column and remove duplicates
+# a. Select the "Industry" column and remove duplicates
 unique_industries <- df %>%
   select(Industry) %>%
   distinct()
@@ -52,48 +52,45 @@ industry_counts <- unique_industries %>%
 
 # Show the top 20 industries
 top_20_industries <- head(industry_counts, 20)
-
-# Print the result
-print(top_20_industries)
+top_20_industries
 
 # b. Top 20 Sectors (must be unique values, no duplicates) 
 # Select the "Sector" column and remove duplicates
 unique_sectors <- df %>%
   select(Sector) %>%
   distinct()
+
 # Count the occurrences of each unique sector
 sector_counts <- unique_sectors %>%
   count(Sector, sort = TRUE)
+
 # Show the top 20 sectors
 top_20_sectors <- head(sector_counts, 20)
-# Print the result
-print(top_20_sectors)
+top_20_sectors
 
 # c. Top 20 Headquarters (must be unique values, no duplicates) 
 # Select the "Headquarter" column and remove duplicates
 unique_headquarters <-df %>%
   select(Headquarters) %>%
   distinct()
+
 # Count the occurrences of each unique headquarter
 headquarter_counts <- unique_headquarters %>%
   count(Headquarters, sort = TRUE)
+
 # Show the top 20 headquarters
 top_20_headquarters <- head(headquarter_counts, 20)
-# Print the result
-print(top_20_headquarters)
+top_20_headquarters
 
-# 6. Then display the following: 
+# SUMMARY 2: Display the following: 
 # a. Top 15 jobs based on Rating
-
 library(dplyr)
 
 top_15_jobs <- df %>%
   arrange(desc(Rating)) %>%  # Sort by Rating in descending order
-  head(15)  # Select the top 15 rows
+  head(15)
 
-# Print the result
-print(top_15_jobs)
-
+top_15_jobs
 
 # b. Top 15 jobs based on Rating under “Consulting” industry 
 top_15_consulting_jobs <- df %>%
@@ -101,14 +98,16 @@ top_15_consulting_jobs <- df %>%
   arrange(desc(Rating)) %>%
   head(15)
 
-print(top_15_consulting_jobs)
+top_15_consulting_jobs
+
 # c. Bottom 15 jobs based on Rating Screenshot the list and paste into a document. 
 bottom_15_jobs <-df %>%
   arrange(Rating) %>%
   head(15)
 
 bottom_15_jobs
-# 7. Then plot the following: 
+
+# SUMMARY 3: Plot the following: 
 # a. Top 10 Companies with rating greater than 3 and under industry “Consulting” 
 library(dplyr)
 library(ggplot2)
@@ -116,7 +115,7 @@ library(ggplot2)
 top_10_consulting_companies <- df %>%
   filter(Industry == "Consulting" & Rating > 3) %>%  # Filter for Consulting and Rating > 3
   arrange(desc(Rating)) %>%  # Sort by Rating in descending order
-  head(10)  # Select the top 10 rows
+  head(10)  
 
 # Create a bar plot
 ggplot(top_10_consulting_companies, aes(x = reorder(`Company Name`, -Rating), y = Rating)) +
@@ -125,12 +124,15 @@ ggplot(top_10_consulting_companies, aes(x = reorder(`Company Name`, -Rating), y 
        x = "Company Name",
        y = "Rating") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for readability
-# In the context of ggplot2, hjust stands for "horizontal justification," and it is used to control the horizontal alignment of text or labels on a plot. hjust takes a numeric value that represents the horizontal justification, where:
+# In the context of ggplot2, hjust stands for "horizontal justification," and it is used to control 
+# the horizontal alignment of text or labels on a plot. hjust takes a numeric value that represents 
+# the horizontal justification, where:
 # hjust = 0 aligns the text to the left.
 # hjust = 0.5 aligns the text to the center.
 # hjust = 1 aligns the text to the right.
-# Display the plot
-ggsave("top_10_consulting_companies_plot.png", width = 10, height = 6)  # Save the plot to a file
+
+# Save the plot to a file
+ggsave("top_10_consulting_companies_plot.png", width = 10, height = 6) 
 
 # b. Top 10 Companies with rating greater than 3 and under industry “Energy”
 top_10_energy_companies <-df %>%
@@ -157,9 +159,8 @@ ggplot(top_10_accounting_companies, aes(x = reorder (`Company Name`, -Rating), y
        x = "Company Name", 
        y = "Rating") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-# 8. Add 10 more summaries/ plots based on your own analysis. 
 
-# 8.1 Distribution of Companies by Size
+# SUMMARY 4: Distribution of Companies by Size
 # Convert the "Size" column
 df$Size <- case_when(
   df$Size == "1 to 50 employees" ~ 1,
@@ -183,7 +184,7 @@ ggplot(df, aes(x = as.factor(Size))) +
        x = "Size",
        y = "Count")
 
-# 8.2 Distribution of Companies by Revenue
+# SUMMARY 5: Distribution of Companies by Revenue
 
 # Decode and convert the "Revenue" column
 df$Revenue <- case_when(
@@ -211,7 +212,45 @@ ggplot(df, aes(x = as.factor(Size))) +
        x = "Revenue",
        y = "Count")
 
-# 8.3 Linear Regression: Revenue vs. Size
+# SUMMARY 6: Top 5 Industries with the highest average rating
+library(dplyr)
+
+top_5_industries <- df %>%
+  group_by(Industry) %>%
+  summarize(Average_Rating = mean(Rating, na.rm = TRUE)) %>%
+  arrange(desc(Average_Rating)) %>%
+  head(5)
+
+top_5_industries
+
+# SUMMARY 7: Top 5 Sectors with the highest average rating 
+top_5_sectors <-df %>%
+  group_by(Sector) %>%
+  summarize(Average_Rating = mean(Rating, na.rm = TRUE)) %>%
+  arrange(desc(Average_Rating)) %>%
+  head(5)
+
+top_5_sectors
+
+# SUMMARY 8: Most common industry per ownership
+library(dplyr)
+most_common_industry_per_ownership <- df %>%
+  filter(!is.na(`Type of ownership`) & !is.na(Industry)) %>%  # Exclude rows with missing values
+  group_by(`Type of ownership`) %>%  # Group by the 'Type of Ownership' column
+  summarize(Most_Common_Industry = names(which.max(table(Industry)))) %>%
+  ungroup()
+most_common_industry_per_ownership
+
+
+# SUMMARY 9: Most common sector per ownership
+most_common_sector_per_ownership <- df %>%
+  filter(!is.na(`Type of ownership`) & !is.na(Sector)) %>%
+  group_by(`Type of ownership`) %>%
+  summarize(Most_Common_Sector = names(which.max(table(Sector)))) %>%
+  ungroup()
+most_common_sector_per_ownership
+
+# Linear Regression 1: Revenue vs. Size
 
 # Create a linear regression model
 model_revenue_size <- lm(Revenue ~ Size, data = df)
@@ -230,7 +269,7 @@ ggplot(df, aes(x = Size, y = Revenue)) +
        y = "Revenue") +
   theme_minimal()
 
-# 8.4 Linear Regression: Founded vs. Size 
+# Linear Regression 2: Founded vs. Size 
 
 # Create a linear regression model
 model_founded_size <- lm(Founded ~ Size, data = df)
@@ -245,7 +284,7 @@ ggplot(df, aes(x = Founded, y = Size)) +
        y = "Size") +
   theme_minimal()
 
-# 8.5 Linear Regression: Founded vs. Rating
+# Linear Regression 3: Founded vs. Rating
 model_founded_rating <- lm(Founded ~ Rating, data = df)
 
 summary(model_founded_rating)
@@ -258,49 +297,9 @@ ggplot(df, aes(x = Founded, y = Rating)) +
        y = "Rating") +
          theme_minimal()
 
-# 8.6 Multiple Regression: Founded, Rating, Size, and Revenue
+# Multiple Regression: Founded, Rating, Size, and Revenue
 multiple_regression_model <- lm(Founded ~ Rating + Size + Revenue, data = df)
 
 # Summary of the multiple regression model
 summary(multiple_regression_model)
-
-# 8.7 Top 5 Industries with the highest average rating
-library(dplyr)
-
-top_5_industries <- df %>%
-  group_by(Industry) %>%
-  summarize(Average_Rating = mean(Rating, na.rm = TRUE)) %>%
-  arrange(desc(Average_Rating)) %>%
-  head(5)
-
-print(top_5_industries)
-
-# 8.8 Top 5 Sectors with the highest average rating 
-
-top_5_sectors <-df %>%
-  group_by(Sector) %>%
-  summarize(Average_Rating = mean(Rating, na.rm = TRUE)) %>%
-  arrange(desc(Average_Rating)) %>%
-  head(5)
-
-print(top_5_sectors)
-
-
-# 8.9 Most common industry per ownership
-library(dplyr)
-most_common_industry_per_ownership <- df %>%
-  filter(!is.na(`Type of ownership`) & !is.na(Industry)) %>%  # Exclude rows with missing values
-  group_by(`Type of ownership`) %>%  # Group by the 'Type of Ownership' column
-  summarize(Most_Common_Industry = names(which.max(table(Industry)))) %>%
-  ungroup()
-most_common_industry_per_ownership
-
-
-# 8.10 Most common sector per ownership
-most_common_sector_per_ownership <- df %>%
-  filter(!is.na(`Type of ownership`) & !is.na(Sector)) %>%
-  group_by(`Type of ownership`) %>%
-  summarize(Most_Common_Sector = names(which.max(table(Sector)))) %>%
-  ungroup()
-most_common_sector_per_ownership
 
